f6c64ac10726828bd34dcba954366f9b24da3994
Ewa Kadziolka <kalistaewa@gmail.com>
2023-05-04 22:22:30 +0200

    serchbar deplaced

--- teddy/src/App.vue
+++ teddy/src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navebar from './components/Navebar.vue';
-import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <header>
     <Navebar/>
   </header>
-  <SearchBar/>
 
   <RouterView />
 </template>
