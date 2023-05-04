93826351459b04849288d9b7bcf9fb04143d40dc
Ewa Kadziolka <kalistaewa@gmail.com>
2023-05-04 02:27:34 +0200

    first commit

---
+++ teddy/src/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid search-bar bg-secondary" style="padding: 35px;">
+            <div class="container">
+                <div class="row g-2">
+                    <div class="col-md-10">
+                        <div class="row g-2">
+                            <div class="col-md-4">
+                                <input type="text" class="form-control border-0 py-3" placeholder="Que cherchez vous?">
+                            </div>
+                            <div class="col-md-4">
+                                <select class="form-select border-0 py-3">
+                                    <option selected>jouets</option>
+                                    <option value="1">livres</option>
+                                    <option value="2">materiel educatif</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <select class="form-select border-0 py-3">
+                                    <option selected>Paris</option>
+                                    <option value="1">Toulouse</option>
+                                    <option value="2">Lyon</option>
+                                    <option value="3">Marseille</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn search-button border-0 w-100 py-3 bg-primary text-light">Chercher</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
---
+++ teddy/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+ <div class="row">
+    <div class="row g-0 align-items-center flex-column-reverse flex-md-row d-flex justify-content-center">
+        <div class="col-md-6 mx-2 d-flex justify-content-center row">
+            <h1 class="display-5 mb-4">Trouve <span class="pacifico text-primary">un jouet ideal</span> pour ton enfant </h1>
+            <p class="pb-2">Ici chaqun peut trouver et echanger, acheter ou vendre les jouets et materiel éducatif.</p>
+            <a href="" class="btn search-button py-3 px-5 me-3 animated fadeIn bg-primary text-light">Get Started</a>
+        </div>
+        <div class="col-md-5">
+                <img class="img-fluid d-flex justify-content-center" src="../assets/images/bear1.png" alt="two teddy-bears">
+            </div>
+        </div>
+  </div>
+
+</template>
