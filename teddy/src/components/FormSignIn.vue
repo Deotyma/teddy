b65c5d415387d51b689260ccd00dfe1d7fa71c90
Ewa Kadziolka <kalistaewa@gmail.com>
2023-05-04 22:25:21 +0200

    add sign in form

---
+++ teddy/src/components/FormSignIn.vue
@@ -0,0 +1,60 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+
+    const logedUser= {
+        email:"",
+        password:""
+    }
+
+    function logInUser() {
+        console.log(logedUser.email)
+        console.log(logedUser.password)
+    }
+
+</script>
+
+<template>
+    
+    <div class="container-fluid py-2" style="padding: 35px;">
+        <div class="col-12 pt-4">
+            <p class="text-center w-100 fs-1">Connectez-vous sur</p>
+            <p class="pacifico text-primary text-center w-100 fs-1">Teddy Blue</p>
+        </div>
+        <div class="row d-flex justify-content-around">
+            <div class="col-md-5 d-sm-none d-md-block">
+                <img class="img-fluid d-flex justify-content-end px-0 mx-0 rounded-3" src="../assets/images/bear2.png" alt="two teddy-bears">
+            </div>
+            <div class="bg-secondary col-md-5 p-5 rounded-3">
+                <form @submit.prevent="logInUser">
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label text-light fw-bolder fs-5">Email</label>
+                        <input v-model="logedUser.email" type="email" class="form-control py-3" id="email" placeholder="name@example.com">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label text-light fw-bolder fs-5">Mot de passe</label>
+                        <input v-model="logedUser.password" type="password" class="form-control py-3" id="password">
+                    </div>
+
+                    <div class="my-3 d-flex justify-content-end">
+                        <a href="#" class="text-primary fw-bolder fs-5">Mot de passe oublié?</a>
+                    </div>
+
+                    <div>
+                        <button type="submit" class="btn search-button border-0 w-100 py-3 bg-primary text-light fw-bolder">Se connecter</button>
+                    </div>
+
+                </form>
+                <div class="row py-3 d-flex justify-content-center">
+                        <div>
+                            <p class="text-center text-light fw-bolder fs-6">Vous découvrez <span class="pacifico">Teddy Blue</span></p>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <RouterLink to="sign-up" class="text-center text-primary fw-bolder fs-6 text-decoration-none">Inscrivez Vous</RouterLink>
+                        </div>
+                    </div>
+            </div>
+        </div> 
+    </div>
+</template>
---
+++ teddy/src/views/SignIn.vue
@@ -0,0 +1,7 @@
+<script setup>
+    import FormSignIn from '../components/FormSignIn.vue';
+</script>
+<template>
+    <FormSignIn/>
+    
+</template>
