4e338f2cb15ed8e6daa90415b0ae8cf6c2863882
Ewa Kadziolka <kalistaewa@gmail.com>
2023-05-30 12:52:14 +0200

    adds form validation for Sign up form

--- teddy/src/components/FormSignUp.vue
+++ teddy/src/components/FormSignUp.vue
@@ -1,17 +1,65 @@
 <script setup>
-
-const createdUser= {
+    import { ref } from 'vue';
+    import { useVuelidate } from '@vuelidate/core';
+    import { required, minLength, maxLength, email,sameAs, helpers} from '@vuelidate/validators'
+const createdUser= ref({
         firstName:"",
         lastName:"",
         email:"",
         password:"",
+        repeatPassword:"",
         address:"",
         zipCode:"",
         town:""
-    }
+    })
 
-    function SignUpUser() {
-        console.log(createdUser)
+const rules = {
+    firstName:{ 
+        required, 
+        minLength:(1)
+    },
+    lastName:{ 
+        required, 
+        minLength:(1)
+    },
+    email: { 
+        required, 
+        email 
+    },
+    password: { 
+        required, 
+        minLength: minLength(8), 
+        maxLength:maxLength(20),
+        hasUppercase: (value) => /[A-Z]/.test(value),
+        hasLowercase: (value) => /[a-z]/.test(value),
+        hasNumber: (value) => /\d/.test(value),
+        hasSymbol: (value) => /[$@$!%*?&]/.test(value), 
+    },
+    //repeatPassword:{
+    //    required,
+    //    sameAsRef: sameAs(createdUser.password)
+    //},
+    address: {
+        required
+    },
+    zipCode: {
+        required
+    },
+    town: {
+        required
+    }
+    };
+
+const v$ = useVuelidate(rules, createdUser);
+
+function SignUpUser() {
+    console.log("password - ", createdUser.password)
+    console.log("données: ",createdUser.value);
+    //console.log(createdUser.repeatPassword)
+        v$.value.$touch();
+    if (!v$.value.$error) {
+        console.log(createdUser.value);
+    }
     }
 
 </script>
@@ -27,40 +75,80 @@ const createdUser= {
                     <div class="row">
                         <div class="col-12 col-md-6 mb-3">
                             <label for="firstName" class="form-label text-light fw-bolder fs-5">Prènom</label>
-                            <input v-model="createdUser.firstName" type="text" class="form-control py-3" id="firstName"/>
+                            <input v-model="createdUser.firstName" type="text" class="form-control py-3" id="firstName" :class="{'is-invalid' : v$.firstName.$error}"/>
+                            <div v-if="v$.firstName.$error">
+                                <p v-for="error of v$.firstName.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
                         </div>
                         <div class="col-12 col-md-6 mb-3">
                             <label for="lastName" class="form-label text-light fw-bolder fs-5">Nom</label>
-                            <input  v-model="createdUser.lastName" type="text" class="form-control py-3" id="lastName"/>
+                            <input  v-model="createdUser.lastName" type="text" class="form-control py-3" id="lastName" :class="{'is-invalid' : v$.lastName.$error}"/>
+                            <div v-if="v$.lastName.$error">
+                                <p v-for="error of v$.lastName.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label text-light fw-bolder fs-5">Email</label>
-                        <input v-model="createdUser.email" type="email" class="form-control py-3" id="email" placeholder="name@example.com">
+                        <input v-model="createdUser.email" type="email" class="form-control py-3" id="email" placeholder="name@example.com" :class="{'is-invalid' : v$.email.$error}">
+                        <div v-if="v$.email.$error">
+                            <p v-for="error of v$.email.$errors" :key="error.$uid" class="text-danger">
+                                {{ error.$message }}
+                            </p>
+                        </div>
                     </div>
                     <div class="col-12 mb-3">
                         <label for="address" class="form-label text-light fw-bolder fs-5">Adresse</label>
-                        <input v-model="createdUser.address" type="text" class="form-control py-3" id="address" placeholder="1, rue Saint Sevrin"/>
+                        <input v-model="createdUser.address" type="text" class="form-control py-3" id="address" placeholder="1, rue Saint Sevrin" :class="{'is-invalid' : v$.address.$error}"/>
+                        <div v-if="v$.address.$error">
+                            <p v-for="error of v$.address.$errors" :key="error.$uid" class="text-danger">
+                                {{ error.$message }}
+                            </p>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col-12 col-md-6 mb-3">
                             <label for="zipCode" class="form-label text-light fw-bolder fs-5">Code postal</label>
-                            <input v-model="createdUser.zipCode" type="text" class="form-control py-3" id="zipCode" placeholder="75009"/>
+                            <input v-model="createdUser.zipCode" type="text" class="form-control py-3" id="zipCode" placeholder="75009" :class="{'is-invalid' : v$.zipCode.$error}"/>
+                            <div v-if="v$.zipCode.$error">
+                                <p v-for="error of v$.zipCode.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
                         </div>
                         <div class="col-12 col-md-6 mb-3">
                             <label for="town" class="form-label text-light fw-bolder fs-5">Ville</label>
-                            <input v-model="createdUser.town" type="text" class="form-control py-3" id="town" placeholder="Paris"/>
+                            <input v-model="createdUser.town" type="text" class="form-control py-3" id="town" placeholder="Paris" :class="{'is-invalid' : v$.town.$error}"/>
+                            <div v-if="v$.town.$error">
+                                <p v-for="error of v$.town.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12 col-md-6 mb-3">
                             <label for="password" class="form-label text-light fw-bolder fs-5">Mot de passe</label>
-                            <input v-model="createdUser.password" type="password" class="form-control py-3" id="password"/>
+                            <input v-model="createdUser.password" type="password" class="form-control py-3" id="password" :class="{'is-invalid' : v$.password.$error}"/>
+                            <div v-if="v$.password.$errors">
+                                <p v-for="error of v$.password.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-6 mb-3">
-                            <label for="repetedPasswprd" class="form-label text-light fw-bolder fs-5">Mot de passe répété</label>
-                            <input type="password" class="form-control py-3" id="repetedPasswprd"/>
-                        </div>
+                       <!--<div class="col-12 col-md-6 mb-3">
+                            <label for="repeatPassword" class="form-label text-light fw-bolder fs-5">Confirmez le mot de passe</label>
+                            <input v-model="createdUser.repeatPassword" type="repeatPassword" class="form-control py-3" id="repeatPassword" :class="{'is-invalid' : v$.repeatPassword.$error}"/>
+                            <div v-if="v$.repeatPassword.$errors">
+                                <p v-for="error of v$.repeatPassword.$errors" :key="error.$uid" class="text-danger">
+                                    {{ error.$message }}
+                                </p>
+                            </div>
+                        </div>-->
                     </div>
                     <div>
                         <button type="submit" class="btn search-button border-0 w-100 py-3 bg-primary text-light fw-bolder">S'inscrire</button>
