0d8a95a0003f9a603eb4090bd45beeaf70d53e36
Ewa Kadziolka <kalistaewa@gmail.com>
2023-11-15 00:19:52 +0100

    DB updated

--- teddyDB/data.dml.sql
+++ teddyDB/data.dml.sql
@@ -2,7 +2,7 @@
 DELETE FROM favorites;
 DELETE FROM annonces;
 DELETE FROM users;
-DELETE FROM shering_methods;
+DELETE FROM sharing_methods;
 DELETE FROM categories;
 DELETE FROM localities;
 
@@ -23,7 +23,7 @@ INSERT INTO categories(category_name)
 	('sport material');
 
 -- Insert data into `shering_methods`
-INSERT INTO shering_methods(shering_method_name) 
+INSERT INTO sharing_methods(sharing_method_name) 
 	VALUES 
 	('looking for'),
 	('exchange'),
@@ -37,15 +37,15 @@ VALUES ('John', 'Doe', 'john_doe', 'john.doe@example.com', '75000', 'Paris', 'pa
        ('Jane', 'Smith', 'jane_smith', 'jane.smith@example.com', '67890', 'Lyon', 'password123');
 
 -- Insert data into `annonces` for `users`, `categories`, and `shering_methods`
-INSERT INTO annonces (title, text_annonce, email, shering_method_name, category_name, date) VALUES
-    ('Old book for sale', 'Montessori book foe sale.', 'john.doe@example.com', 'sell', 'book', '2023-11-02'),
+INSERT INTO annonces (title, text_annonce, email, sharing_method_name, category_name, date) VALUES
+    ('Old book for sale', 'Montessori book for sale.', 'john.doe@example.com', 'sell', 'book', '2023-11-02'),
     ('Kids toy', 'Puzzle of 3 elements to give.', 'mdupont@example.com', 'give', 'toy', '2023-11-03');
 
 
 
 
-INSERT INTO favorites(id_annonce, email) 
-VALUES ((SELECT a.id_annonce FROM annonces a WHERE a.title = 'Old book for sale'),
+INSERT INTO favorites(id, email) 
+VALUES ((SELECT a.id FROM annonces a WHERE a.title = 'Old book for sale'),
         (SELECT u.email FROM users u WHERE u.nick_name = 'mdupont')),
-       ((SELECT a.id_annonce FROM annonces a WHERE a.title = 'Kids toy'),
+       ((SELECT a.id FROM annonces a WHERE a.title = 'Kids toy'),
         (SELECT u.email FROM users u WHERE u.nick_name = 'jane_smith'));
--- teddyDB/schema.ddl.sql
+++ teddyDB/schema.ddl.sql
@@ -2,57 +2,57 @@
 DROP TABLE IF EXISTS favorites;
 DROP TABLE IF EXISTS annonces;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS shering_methods;
+DROP TABLE IF EXISTS sharing_methods;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS localities;
 
+-- Creating tables
 CREATE TABLE localities (
-zip_code VARCHAR (5) NOT NULL,
-locality_name VARCHAR(150) NOT NULL,
-PRIMARY KEY (zip_code, locality_name)
-
+    zip_code VARCHAR (5) NOT NULL,
+    locality_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (zip_code, locality_name)
 );
 
 CREATE TABLE categories (
-	category_name VARCHAR(50) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE shering_methods (
-	shering_method_name VARCHAR(15) PRIMARY KEY
+CREATE TABLE sharing_methods (
+    id SERIAL PRIMARY KEY,
+    sharing_method_name VARCHAR(15) UNIQUE NOT NULL
 );
 
 CREATE TABLE users (
- 	email VARCHAR(255) PRIMARY KEY UNIQUE,
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     nick_name VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     zip_code VARCHAR(5) NOT NULL,
     locality_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (zip_code, locality_name) REFERENCES localities(zip_code, locality_name) ON DELETE CASCADE
+     FOREIGN KEY (zip_code, locality_name) REFERENCES localities(zip_code, locality_name) ON DELETE CASCADE
 );
 
-
 CREATE TABLE annonces (
-    id_annonce SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     text_annonce TEXT NOT NULL,
     photo_link VARCHAR(255),
     email VARCHAR(255),
-    shering_method_name VARCHAR(15),
+    sharing_method_name VARCHAR(15),
     category_name VARCHAR(50),
     date DATE NOT NULL,
-    FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE,
-    FOREIGN KEY (shering_method_name) REFERENCES shering_methods(shering_method_name) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (sharing_method_name) REFERENCES sharing_methods(sharing_method_name) ON DELETE CASCADE,  -- Corrected the reference name
     FOREIGN KEY (category_name) REFERENCES categories(category_name) ON DELETE CASCADE
 );
 
-
 CREATE TABLE favorites (
     email VARCHAR(255),
-    id_annonce INT NOT NULL,
-    PRIMARY KEY (email, id_annonce),
+    id INT,
+    PRIMARY KEY (id, email),
     FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE,
-    FOREIGN KEY (id_annonce) REFERENCES annonces(id_annonce) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES annonces(id) ON DELETE CASCADE
 );
-
