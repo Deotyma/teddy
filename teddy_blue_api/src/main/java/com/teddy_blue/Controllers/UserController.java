9b16192eaa8dd28cbdcf7163c6fe6e8c2db9f823
Ewa Kadziolka <kalistaewa@gmail.com>
2023-11-15 00:19:31 +0100

    User added

--- teddy_blue_api/src/main/java/com/teddy_blue/Controllers/UserController.java
+++ teddy_blue_api/src/main/java/com/teddy_blue/Controllers/UserController.java
@@ -3,7 +3,7 @@ package com.teddy_blue.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teddy_blue.Entities.User;
@@ -28,38 +27,37 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Create a new user
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody User user) {
-        return userService.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
     }
 
+    // Retrieve a user by ID
     @GetMapping("/{id}")
-    public User  getUserById(@PathVariable Long id) {
-        return userService.findById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id)
+                     .orElseThrow();
+        return ResponseEntity.ok(user);
     }
 
+    // Retrieve all users
     @GetMapping
-    public List<User > getAllUsers() {
-        return userService.findAll();
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
+    // Update a user
     @PutMapping("/{id}")
-    public User updateUser(@PathVariable Long idUser, @RequestBody User
-	    user) {
-        if(!idUser.equals(user.getIdUser())) {
-            throw new RuntimeException("User ID mismatch");
-        }
-        return userService.save(user);
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User updatedUser = userService.updateUser(id, userDetails);
+        return ResponseEntity.ok(updatedUser);
     }
 
+    // Delete a user
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteById(id);
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
     }
 }
-
-
-
-
