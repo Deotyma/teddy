a252ee854b34fdb0d31fc5d2c07b017cb56e633e
Deotyma <kalistaewa@gmail.com>
2024-02-10 16:08:02 +0100

    update of axios

--- teddy_front/src/main.js
+++ teddy_front/src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n/i18n.js'
-
+import axiosConfig from "./plugins/axiosConfig.js";
 const app = createApp(App)
 
-app.use(router)
+app.use(router),
+app.use(axiosConfig);
 
 app.use(i18n)
 
